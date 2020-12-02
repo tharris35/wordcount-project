c4f05d6ea4ce1bff8b474218c4966914a2e8210d
tharris35 <49250384+tharris35@users.noreply.github.com>
2020-12-02 14:48:37 -0500

    Add files via upload

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -1,7 +1,7 @@
 """wordcount URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.homepage, name='home'),
+    path('count/', views.count, name='count'),
+    path('about/', views.about, name='about'),
 ]
